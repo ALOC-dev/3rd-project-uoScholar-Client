683854dd98438ff613b2cbe2e5ab9029bf3c3cfc
greengrape777 <junghoonkim0418@gmail.com>
2025-03-31 18:29:42 +0900

    refactor: API 요청 POST 형식 변경

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -3,8 +3,12 @@ import axios from 'axios';
 export const sendTextToBackend = async (text: string) => {
     const API_URL = '';
 
+    const payload = {
+        message: text
+    }
+
     try {
-        const response = await axios.post(API_URL, { text });
+        const response = await axios.post(API_URL, payload);
         return response.data.response;
     } catch (error) {
         console.error('API 요청 실패:', error);
