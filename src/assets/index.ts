df4c7acf33029373cb2d93f6fb38ebfda9fd4ee9
greengrape777 <junghoonkim0418@gmail.com>
2025-04-07 21:13:17 +0900

    HomeScreen 화면 모음 구성 완료료

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,8 @@
 const IMAGES = {
     TABICON: require('./images/tabIcon.png'),
     BACKWARD: require('./images/backward.png'),
+    UNSEND: require('./images/unsend.png'),
+    SEND: require('./images/send.png'),
 }
 
 export default IMAGES;
--- src/screens/main/HomeScreen/ChatInput.tsx
+++ src/screens/main/HomeScreen/ChatInput.tsx
@@ -4,35 +4,69 @@ import {
     StyleSheet,
     TouchableOpacity,
     TextInput,
-    Button,
+    Image,
 } from "react-native";
+import IMAGES from "../../../assets";
 
 const ChatInput = () => {
-    const [text, setText] = useState(''); // user input
-    
+    const [text, setText] = useState('');
+
+    const handleSend = () => {
+        if (text === '') return;
+        console.log('전송:', text);
+
+        
+
+        setText('');
+    }
+
     return (
         <View style={styles.container}>
-            <TextInput
-                style={styles.input}
-                placeholder="텍스트를 입력하세요"
-                value={text}
-                onChangeText={setText}
-            />
+            <View style={styles.inputRow}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="텍스트를 입력하세요"
+                    value={text}
+                    onChangeText={setText}
+                />
+                <TouchableOpacity onPress={handleSend}>
+                    <Image
+                        source={
+                            text === ''
+                                ? IMAGES.UNSEND
+                                : IMAGES.SEND
+                        }
+                        style={styles.icon}
+                    />
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        padding: 1,
+    },
+    inputRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
     },
     input: {
         flexShrink: 1,
+        flex: 1,
         borderWidth: 1,
-        borderColor: '#E0E0E0',
-        paddingHorizontal: 10,
-        height: 50,
-    },    
+        borderColor: '#ccc',
+        height: 40,
+        borderRadius: 25,
+        paddingVertical: 0,
+        paddingLeft: 10,
+    },
+    icon: {
+        width: 40,
+        height: 40,
+        marginLeft: 8,
+    },
 });
 
 export default ChatInput;
